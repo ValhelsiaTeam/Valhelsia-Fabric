7ea694d9b92eed812b63c784975e826e5c440e61
jangro <scott@jangro.com>
2021-08-24 07:13:02 -0600

    Update recipes.js
    removed string -> cobweb

--- kubejs/server_scripts/recipes.js
+++ kubejs/server_scripts/recipes.js
@@ -2,6 +2,5 @@
 onEvent('recipes', event => {
 	// Change recipes here
     event.shapeless('4x minecraft:string','minecraft:cobweb')
-    event.shapeless('minecraft:cobweb', '9x minecraft:string')
 })
 
