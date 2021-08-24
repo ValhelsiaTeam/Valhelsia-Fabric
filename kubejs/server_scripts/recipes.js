f7f42005c95e1fe4be1ca7f97ca3ea44fc97897b
jangro <scott@jangro.com>
2021-08-24 07:03:26 -0600

    Create recipes.js
    For missing recipes

---
+++ kubejs/server_scripts/recipes.js
@@ -0,0 +1,7 @@
+
+onEvent('recipes', event => {
+	// Change recipes here
+    event.shapeless('4x minecraft:string','minecraft:cobweb')
+    event.shapeless('minecraft:cobweb', '9x minecraft:string')
+})
+
