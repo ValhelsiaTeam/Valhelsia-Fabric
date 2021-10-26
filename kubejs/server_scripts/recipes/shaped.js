8251440f0fab24a22a59734a8f72ef56a65be68e
Khytwel <54046029+Khytwel@users.noreply.github.com>
2021-10-26 15:09:13 +0200

    Some improvements.

--- kubejs/server_scripts/recipes/shaped.js
+++ kubejs/server_scripts/recipes/shaped.js
@@ -9,5 +9,54 @@ events.listen('recipes', function (event) {
   ], {
     A: 'minecraft:string'
   })
-
+  
+  // Alloy Forgery
+  event.shaped(Item.of('alloy_forgery:deepslate_bricks_forge_controller', 1), [
+    'AAA',
+    'ABA',
+    'AAA'
+  ], {
+    A: 'minecraft:deepslate_bricks',
+    B: 'alloy_forgery:bricks_forge_controller'
+  })
+  event.shaped(Item.of('alloy_forgery:deepslate_bricks_forge_controller', 1), [
+    'AAA',
+    'ABA',
+    'AAA'
+  ], {
+    A: 'minecraft:deepslate_bricks',
+    B: 'alloy_forgery:stone_bricks_forge_controller'
+  })
+  event.shaped(Item.of('alloy_forgery:polished_blackstone_forge_controller', 1), [
+    'AAA',
+    'ABA',
+    'AAA'
+  ], {
+    A: 'minecraft:polished_blackstone',
+    B: 'alloy_forgery:bricks_forge_controller'
+  })
+  event.shaped(Item.of('alloy_forgery:polished_blackstone_forge_controller', 1), [
+    'AAA',
+    'ABA',
+    'AAA'
+  ], {
+    A: 'minecraft:polished_blackstone',
+    B: 'alloy_forgery:stone_bricks_forge_controller'
+  })
+  event.shaped(Item.of('alloy_forgery:end_stone_bricks_forge_controller', 1), [
+    'AAA',
+    'ABA',
+    'AAA'
+  ], {
+    A: 'minecraft:end_stone_bricks',
+    B: 'alloy_forgery:polished_blackstone_forge_controller'
+  })
+  event.shaped(Item.of('alloy_forgery:end_stone_bricks_forge_controller', 1), [
+    'AAA',
+    'ABA',
+    'AAA'
+  ], {
+    A: 'minecraft:end_stone_bricks',
+    B: 'alloy_forgery:deepslate_bricks_forge_controller'
+  })
 })
